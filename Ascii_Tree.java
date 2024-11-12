1afdfcf380e04ebe7bda16f8d89db9e5bb9a59e3
alannaramm <110500317+alannaramm@users.noreply.github.com>
2024-11-12 18:08:44 -0500

    Update Ascii_Tree.java

--- Ascii_Tree.java
+++ Ascii_Tree.java
@@ -1,23 +1,32 @@
 
 public class Ascii_Tree {
 
-	public static void main(String[] args) {
-		//       
-	       for(int i = 1; i <= 10; i++) {
-	           for(int j = 10; j >= i ; j--) {
-	               System.out.print(" ");
-	           }
-	           for(int A = 1; A < A ; A++) {
-	               System.out.print("\\");
-	           }
-	           System.out.print("|");
-	           for(int H = 1; H < H ; H++)
-	               System.out.print("/");
-	           System.out.println("");
-	       }
-		
-		
-		
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            // Print leading spaces
+            for (int j = 10; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            // Print left slashes
+            for (int A = 1; A < i; A++) {
+                System.out.print("\\");
+            }
+
+            // Print trunk
+            System.out.print("|");
+
+            // Print right slashes
+            for (int H = 1; H < i; H++) {
+                System.out.print("/");
+            }
+
+            // Move to the next line
+            System.out.println();
+        }
+    }
+}
+
 		
 		
 		
