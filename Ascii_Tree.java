63e92ce8c84293349b2206b98340f472245feaff
alannaramm <110500317+alannaramm@users.noreply.github.com>
2024-11-12 18:06:13 -0500

    Add files via upload

---
+++ Ascii_Tree.java
@@ -0,0 +1,29 @@
+
+public class Ascii_Tree {
+
+	public static void main(String[] args) {
+		//       
+	       for(int i = 1; i <= 10; i++) {
+	           for(int j = 10; j >= i ; j--) {
+	               System.out.print(" ");
+	           }
+	           for(int A = 1; A < A ; A++) {
+	               System.out.print("\\");
+	           }
+	           System.out.print("|");
+	           for(int H = 1; H < H ; H++)
+	               System.out.print("/");
+	           System.out.println("");
+	       }
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
